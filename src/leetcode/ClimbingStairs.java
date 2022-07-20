5deda6371747d37c30030189654e4d7f166fb5d2
1104063 <jerry.zeng@iisigroup.com>
2022-07-20 15:38:02 +0800

    ClimbingStairs

---
+++ src/leetcode/ClimbingStairs.java
@@ -0,0 +1,36 @@
+package leetcode;
+
+public class ClimbingStairs {
+
+  public static void main(String[] args) {
+      System.out.println(climbStairs(45));
+      System.out.println(climbStairs2(45));
+  }
+
+    public static int climbStairs(int n) {
+         if(n==1){
+             return 1;
+         }
+         if(n==2){
+             return 2;
+         }
+         return climbStairs(n-1)+climbStairs(n-2);
+    }
+
+    public static int climbStairs2(int n) {
+        int[] dp = new int[n + 1];
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i-1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+}
